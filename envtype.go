feb9e89ad1d8896a80ff02acaf75f30d95d4d37b
unknown <harits.fahreza@gmail.com>
2018-06-28 14:11:53 +0700

    Change envtype to Midtrans domain
    Change envtype URL from veritrans.co.id to midtrans.com

--- envtype.go
+++ envtype.go
@@ -5,26 +5,26 @@ import "strings"
 type EnvironmentType int8
 
 const (
-    _ EnvironmentType = iota
-    Sandbox
-    Production
+	_ EnvironmentType = iota
+	Sandbox
+	Production
 )
 
-var typeString = map[EnvironmentType]string {
-    Sandbox: "https://api.sandbox.veritrans.co.id",
-    Production: "https://api.veritrans.co.id",
+var typeString = map[EnvironmentType]string{
+	Sandbox:    "https://api.sandbox.midtrans.com",
+	Production: "https://api.midtrans.com",
 }
 
 // implement stringer
 func (e EnvironmentType) String() string {
-    for k, v := range typeString {
-        if k == e {
-            return v
-        }
-    }
-    return "undefined"
+	for k, v := range typeString {
+		if k == e {
+			return v
+		}
+	}
+	return "undefined"
 }
 
 func (e EnvironmentType) SnapUrl() string {
-    return strings.Replace(e.String(), "api.", "app.", 1)
+	return strings.Replace(e.String(), "api.", "app.", 1)
 }
