b5881d5ae63090c5834abc09183b19d6bc0d62fa
unknown <harits.fahreza@gmail.com>
2018-06-28 14:14:51 +0700

    Change midtrans package import
    Change simplepay main.go and some tests on how they import midtrans pkg

--- example/simplepay/main.go
+++ example/simplepay/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-    "github.com/saveav/go-midtrans"
-    "flag"
-    "fmt"
-    "net/http"
-    "log"
-    "time"
-    "strconv"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	midtrans "github.com/veritrans/go-midtrans"
 )
 
 var midclient midtrans.Client
@@ -15,65 +16,65 @@ var coreGateway midtrans.CoreGateway
 var snapGateway midtrans.SnapGateway
 
 func main() {
-    setupMidtrans()
-    var addr = flag.String("port", ":1234", "The address of the application")
-    flag.Parse()
-    fmt.Println("Server started on port: ", *addr)
+	setupMidtrans()
+	var addr = flag.String("port", ":1234", "The address of the application")
+	flag.Parse()
+	fmt.Println("Server started on port: ", *addr)
 
-    http.Handle("/", &templateHandler{filename: "index.html"})
-    http.Handle("/snap", &templateHandler{
-        filename: "snap_index.html",
-        dataInitializer: func (t *templateHandler) {
-            snapResp, err := snapGateway.GetTokenQuick(generateOrderId(), 200000)
-            t.data = make(map[string]interface{})
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	http.Handle("/snap", &templateHandler{
+		filename: "snap_index.html",
+		dataInitializer: func(t *templateHandler) {
+			snapResp, err := snapGateway.GetTokenQuick(generateOrderId(), 200000)
+			t.data = make(map[string]interface{})
 
-            if err != nil {
-                log.Fatal("Error generating snap token: ", err)
-                t.data["Token"] = ""
-            } else {
-                t.data["Token"] = snapResp.Token
-            }
-        },
-    })
-    http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-    http.HandleFunc("/chargeDirect", chargeDirect)
+			if err != nil {
+				log.Fatal("Error generating snap token: ", err)
+				t.data["Token"] = ""
+			} else {
+				t.data["Token"] = snapResp.Token
+			}
+		},
+	})
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/chargeDirect", chargeDirect)
 
-    if err := http.ListenAndServe(*addr, nil); err != nil {
-        log.Fatal("Failed starting server: ", err)
-    }
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("Failed starting server: ", err)
+	}
 }
 
 func setupMidtrans() {
-    midclient = midtrans.NewClient()
-    midclient.ServerKey = "VT-server-7CVlR3AJ8Dpkez3k_TeGJQZU"
-    midclient.ClientKey = "VT-client-IKktHiy3aRYHljsw"
-    midclient.ApiEnvType = midtrans.Sandbox
+	midclient = midtrans.NewClient()
+	midclient.ServerKey = "VT-server-7CVlR3AJ8Dpkez3k_TeGJQZU"
+	midclient.ClientKey = "VT-client-IKktHiy3aRYHljsw"
+	midclient.ApiEnvType = midtrans.Sandbox
 
-    coreGateway = midtrans.CoreGateway{
-        Client: midclient,
-    }
+	coreGateway = midtrans.CoreGateway{
+		Client: midclient,
+	}
 
-    snapGateway = midtrans.SnapGateway{
-        Client: midclient,
-    }
+	snapGateway = midtrans.SnapGateway{
+		Client: midclient,
+	}
 }
 
 func chargeDirect(w http.ResponseWriter, r *http.Request) {
-    chargeResp, _ := coreGateway.Charge(&midtrans.ChargeReq{
-        PaymentType: midtrans.SourceCreditCard,
-        CreditCard: &midtrans.CreditCardDetail{
-            TokenID: r.FormValue("card-token"),
-        },
-        TransactionDetails: midtrans.TransactionDetails{
-            OrderID: generateOrderId(),
-            GrossAmt: 200000,
-        },
-    })
+	chargeResp, _ := coreGateway.Charge(&midtrans.ChargeReq{
+		PaymentType: midtrans.SourceCreditCard,
+		CreditCard: &midtrans.CreditCardDetail{
+			TokenID: r.FormValue("card-token"),
+		},
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  generateOrderId(),
+			GrossAmt: 200000,
+		},
+	})
 
-    fmt.Println(chargeResp.ValMessages)
-    fmt.Println(chargeResp.StatusMessage)
+	fmt.Println(chargeResp.ValMessages)
+	fmt.Println(chargeResp.StatusMessage)
 }
 
 func generateOrderId() string {
-    return strconv.FormatInt(time.Now().UnixNano(), 10)
+	return strconv.FormatInt(time.Now().UnixNano(), 10)
 }
--- request_test.go
+++ request_test.go
@@ -1,42 +1,43 @@
 package midtrans_test
 
 import (
-    "midtrans"
-    "testing"
-    "github.com/cheekybits/is"
-    "encoding/json"
+	"encoding/json"
+	"testing"
+
+	"github.com/cheekybits/is"
+	midtrans "github.com/veritrans/go-midtrans"
 )
 
 func TestCustomerFieldOmmittable(t *testing.T) {
-    is := is.New(t)
+	is := is.New(t)
 
-    req := midtrans.ChargeReq{
-        PaymentType: midtrans.SourceCreditCard,
-    }
+	req := midtrans.ChargeReq{
+		PaymentType: midtrans.SourceCreditCard,
+	}
 
-    reqJSON, _ := json.Marshal(req)
-    is.Equal(string(reqJSON), `{"payment_type":"credit_card","transaction_details":{"order_id":"","gross_amount":0}}`)
+	reqJSON, _ := json.Marshal(req)
+	is.Equal(string(reqJSON), `{"payment_type":"credit_card","transaction_details":{"order_id":"","gross_amount":0}}`)
 
-    req.CustField1 = "f1"
-    req.CustField2 = "f2"
-    req.CustField3 = "f3"
-    reqJSON, _ = json.Marshal(req)
-    is.Equal(string(reqJSON), `{"payment_type":"credit_card","transaction_details":{"order_id":"","gross_amount":0},"custom_field1":"f1","custom_field2":"f2","custom_field3":"f3"}`)
+	req.CustField1 = "f1"
+	req.CustField2 = "f2"
+	req.CustField3 = "f3"
+	reqJSON, _ = json.Marshal(req)
+	is.Equal(string(reqJSON), `{"payment_type":"credit_card","transaction_details":{"order_id":"","gross_amount":0},"custom_field1":"f1","custom_field2":"f2","custom_field3":"f3"}`)
 }
 
 func TestBankTransferMandiriBill(t *testing.T) {
-    is := is.New(t)
+	is := is.New(t)
 
-    req := midtrans.ChargeReq{
-        PaymentType: midtrans.SourceBankTransfer,
-        BankTransfer: &midtrans.BankTransferDetail{
-            MandiriBillBankTransferDetail: &midtrans.MandiriBillBankTransferDetail{
-                BillInfo1: "Silahkan transfer",
-                BillInfo2: "Untuk pembelian pulsa",
-            },
-        },
-    }
+	req := midtrans.ChargeReq{
+		PaymentType: midtrans.SourceBankTransfer,
+		BankTransfer: &midtrans.BankTransferDetail{
+			MandiriBillBankTransferDetail: &midtrans.MandiriBillBankTransferDetail{
+				BillInfo1: "Silahkan transfer",
+				BillInfo2: "Untuk pembelian pulsa",
+			},
+		},
+	}
 
-    reqJSON, _ := json.Marshal(req)
-    is.Equal(string(reqJSON), `{"payment_type":"bank_transfer","transaction_details":{"order_id":"","gross_amount":0},"bank_transfer":{"bill_info1":"Silahkan transfer","bill_info2":"Untuk pembelian pulsa"}}`)
+	reqJSON, _ := json.Marshal(req)
+	is.Equal(string(reqJSON), `{"payment_type":"bank_transfer","transaction_details":{"order_id":"","gross_amount":0},"bank_transfer":{"bill_info1":"Silahkan transfer","bill_info2":"Untuk pembelian pulsa"}}`)
 }
