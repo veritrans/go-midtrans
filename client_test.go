d9c27f41117b926897867cdd9c7d43ab6e65014c
Rizda Dwi Prasetya <rizda.prasetya@midtrans.com>
2019-03-05 18:27:36 +0700

    add test for snapGateway.GetTokenQuick

--- client_test.go
+++ client_test.go
@@ -3,6 +3,10 @@ package midtrans_test
 import (
 	"testing"
 
+	"time"
+
+	"strconv"
+
 	midtrans "github.com/veritrans/go-midtrans"
 
 	"github.com/cheekybits/is"
@@ -13,4 +17,27 @@ func TestDefaultEnvironmentType(t *testing.T) {
 
 	midclient := midtrans.NewClient()
 	is.Equal(midtrans.Sandbox, midclient.APIEnvType)
+}
+
+func TestSnapCreateTokenQuick(t *testing.T) {
+	is := is.New(t)
+    now := time.Now()
+    timestamp := strconv.FormatInt(now.Unix(), 10)
+
+    midclient := midtrans.NewClient()
+    midclient.ServerKey = "SB-Mid-server-GwUP_WGbJPXsDzsNEBRs8IYA"
+    midclient.ClientKey = "SB-Mid-client-61XuGAwQ8Bj8LxSS"
+    midclient.APIEnvType = midtrans.Sandbox
+
+	var snapGateway midtrans.SnapGateway
+	snapGateway = midtrans.SnapGateway{
+		Client: midclient,
+	}
+
+	snapResp, err := snapGateway.GetTokenQuick("order-id-go-"+timestamp, 200000)
+	var snapToken string
+	if err != nil {
+		snapToken = snapResp.Token
+		is.OK(snapToken)
+	}
 }
