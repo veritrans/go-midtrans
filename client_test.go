b99c0c69af8bce0f9a5680615c627794a6920847
Rizda Dwi Prasetya <rizdaprasetya@users.noreply.github.com>
2019-03-12 14:20:27 +0700

    fix test

--- client_test.go
+++ client_test.go
@@ -21,13 +21,13 @@ func TestDefaultEnvironmentType(t *testing.T) {
 
 func TestSnapCreateTokenQuick(t *testing.T) {
 	is := is.New(t)
-    now := time.Now()
-    timestamp := strconv.FormatInt(now.Unix(), 10)
+	now := time.Now()
+	timestamp := strconv.FormatInt(now.Unix(), 10)
 
-    midclient := midtrans.NewClient()
-    midclient.ServerKey = "SB-Mid-server-GwUP_WGbJPXsDzsNEBRs8IYA"
-    midclient.ClientKey = "SB-Mid-client-61XuGAwQ8Bj8LxSS"
-    midclient.APIEnvType = midtrans.Sandbox
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = "SB-Mid-server-GwUP_WGbJPXsDzsNEBRs8IYA"
+	midclient.ClientKey = "SB-Mid-client-61XuGAwQ8Bj8LxSS"
+	midclient.APIEnvType = midtrans.Sandbox
 
 	var snapGateway midtrans.SnapGateway
 	snapGateway = midtrans.SnapGateway{
@@ -37,6 +37,8 @@ func TestSnapCreateTokenQuick(t *testing.T) {
 	snapResp, err := snapGateway.GetTokenQuick("order-id-go-"+timestamp, 200000)
 	var snapToken string
 	if err != nil {
+		
+	} else {
 		snapToken = snapResp.Token
 		is.OK(snapToken)
 	}
